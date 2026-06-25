8a25359381ff3b59ad562cad220133b543e27483
Olusammi <samuelhassan33@gmail.com>
2026-06-25 13:36:28 +0100

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ st.set_page_config(
     page_title="LAMATA Emissions Portal",
     page_icon="🚌",
     layout="wide",
-    initial_sidebar_state="expanded",
+    initial_sidebar_state="expanded"
 )
 
 # ════════════════════════════════════════════════════════
